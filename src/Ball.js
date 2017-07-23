a974160a32e773092dafde84f8b25552f076f6a0
dasilvafrederic <frederic.dasilva.pro@gmail.com>
2017-07-23 20:22:58 +0200

    first animation

---
+++ src/Ball.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
+
+export default class Ball extends Component {
+  state = { }
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: { x: 300, y: 500 },
+    }).start();
+  }
+  render() {
+    return (
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  ball: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    borderWidth: 30,
+    borderColor: 'black',
+  },
+});
